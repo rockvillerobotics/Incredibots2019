c077a22792f7ce169394a99dbe397f06a2800221
JawadH02 <44210615+JawadH02@users.noreply.github.com>
2019-12-11 16:24:38 -0500

    Update actions.py

--- Roomba/actions.py
+++ Roomba/actions.py
@@ -12,20 +12,13 @@ def first_position():
     g.forwards_gyro_until_black_rfcliff()
     g.forwards_gyro_until_white_rfcliff(0)
     g.forwards_gyro_until_black_rfcliff(0)
-    #s.align_close_fcliffs()
     msleep(100)
     g.turn_left_gyro()
     g.forwards_gyro_until_black_lfcliff()
-    g.forwards_gyro(700)
-    g.backwards_gyro_until_black_rfcliff()
-    g.turn_left_gyro()
-    s.lfollow_lfcliff_until_lfcliff_senses_black_pid()
-    s.forwards_gyro_until_white_lfcliff()
-    g.forwards_gyro(600)
-    g.turn_right_gyro()
-    s.forwards_gyro_until_bump()
-    
-        
+    g.forwards_gyro(500)
+    g.turn_left_gyro(60)
+    s.lfollow_lfcliff_until_rfcliff_senses_black_pid()
+    g.turn_left_gyro(6)
     
     
     
